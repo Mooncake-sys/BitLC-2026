29beb4a4a1bab25222c92d8e3d633639a06b3608
ΜΩΩΠζΛΚΞ <schwagereit.katharina@gmail.com>
2026-02-10 15:26:16 +0100

    Add files via upload

---
+++ hangman.pas
@@ -0,0 +1,139 @@
+(*Erstellen Sie ein Programm „Hang-Man“: Ein Wort wird in einem Array eingegeben, der andere 
+Mitspieler gibt nun einen Buchstaben ein. Nach jeder Eingabe wird der Rateerfolg angezeigt.
+Beispiel:
+Eingabe: a -a--
+Eingabe: u -au
+Eingabe: s -aus
+Eingabe: m mau*)
+
+program hangman;
+
+uses
+  sysutils;
+
+var
+  word: array of char;
+  wordLength, i, attempts, guessedCount: integer;
+  guess: char;
+  inputWord, guessInput: string;
+  found: boolean;
+  display: array of char;
+  guessedLetters: array of char;
+  guessCount: integer;
+
+begin
+  randomize;
+  
+  //Spieler 1: Wort eingabe
+  writeln('=== HANGMAN SPIEL ===');
+  writeln;
+  writeln('Spieler 1: Gib ein Wort ein:');
+  readln(inputWord);
+  
+  //Eingegebenes Wort im Array speichern
+  wordLength := length(inputWord);
+  SetLength(word, wordLength);
+  SetLength(display, wordLength);
+  SetLength(guessedLetters, 26);
+  
+  for i := 0 to wordLength - 1 do
+  begin
+    word[i] := inputWord[i + 1];
+    display[i] := '-';  //Initialien aller Buchstaben verbergen
+  end;
+  
+  guessCount := 0;
+  attempts := 9;
+  guessedCount := 0;
+  
+  writeln;
+  writeln('=== Spieler 2 beginnt zu raten ===');
+  writeln('Das Wort hat ', wordLength, ' Buchstaben.');
+  writeln('Du hast 9 Versuche!');
+  writeln;
+  
+  //Spielschleife 
+  while (attempts > 0) and (guessedCount < wordLength) do
+  begin
+    //Aktuellen Status anzeigen
+    write('Wort: ');
+    for i := 0 to wordLength - 1 do
+      write(display[i]);
+    writeln;
+    writeln('Verbleibende Versuche: ', attempts);
+    writeln;
+    
+    //Buchstabe eingabe
+    write('Rate einen Buchstaben: ');
+    readln(guessInput);
+    
+    if length(guessInput) < 1 then
+    begin
+      writeln('Bitte einen Buchstaben eingeben!');
+      writeln;
+      continue;
+    end;
+    
+    guess := guessInput[1];
+    
+    //Prüfe ob Buchstabe bereits geraten wurde
+    found := false;
+    for i := 0 to guessCount - 1 do
+    begin
+      if guessedLetters[i] = guess then
+      begin
+        writeln('Diesen Buchstaben hast du bereits geraten!');
+        writeln;
+        found := true;
+        break;
+      end;
+    end;
+    
+    if found then
+      continue;
+    
+    //Buchstabe zu geratener Liste hinzufügen
+    guessedLetters[guessCount] := guess;
+    inc(guessCount);
+    
+    //Buchstabe im Wort suchen 
+    found := false;
+    for i := 0 to wordLength - 1 do
+    begin
+      if word[i] = guess then
+      begin
+        display[i] := guess;
+        found := true;
+        inc(guessedCount);
+      end;
+    end;
+    
+    if found then
+      writeln('Richtig! Der Buchstabe ist im Wort!')
+    else
+    begin
+      writeln('Leider falsch!');
+      dec(attempts);
+    end;
+    
+    writeln;
+  end;
+  
+  //Spielende
+  writeln('=== SPIELENDE ===');
+  write('Das Wort war: ');
+  for i := 0 to wordLength - 1 do
+    write(word[i]);
+  writeln;
+  writeln;
+  
+  if guessedCount = wordLength then
+    writeln('Glückwunsch! Du hast gewonnen!')
+  else
+  begin
+    writeln('Game Over! Du hast verloren.');
+    writeln('Verbleibende Versuche: ', attempts);
+  end;
+  
+  readln;
+end.
